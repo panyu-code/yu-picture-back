aedb93f532a1041154b6285721765138f5661f3b
yupan <panyu_lucky9527@163.com>
2024-12-22 21:28:20 +0800

    图片模块上传功能开发

--- sql/init_sql.sql
+++ sql/init_sql.sql
@@ -19,3 +19,42 @@ create table user
 )
     comment '用户表';
 
+
+create table picture
+(
+    id           bigint auto_increment comment 'id'
+        primary key,
+    url          varchar(512)                       not null comment '图片 url',
+    name         varchar(128)                       not null comment '图片名称',
+    introduction varchar(512)                       null comment '简介',
+    category     varchar(64)                        null comment '分类',
+    tags         varchar(512)                       null comment '标签（JSON 数组）',
+    size         bigint                             null comment '图片体积',
+    width        int                                null comment '图片宽度',
+    height       int                                null comment '图片高度',
+    scale        double                             null comment '图片宽高比例',
+    format       varchar(32)                        null comment '图片格式',
+    user_id      bigint                             not null comment '创建用户 id',
+    create_time  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete    tinyint  default 0                 not null comment '是否删除'
+)
+    comment '图片表' collate = utf8mb4_unicode_ci;
+
+create index idx_category
+    on picture (category);
+
+create index idx_introduction
+    on picture (introduction);
+
+create index idx_name
+    on picture (name);
+
+create index idx_tags
+    on picture (tags);
+
+create index idx_userId
+    on picture (user_id);
+
+
+
