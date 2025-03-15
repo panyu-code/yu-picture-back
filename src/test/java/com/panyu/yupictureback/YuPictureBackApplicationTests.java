557c59cfe12b707f465544dd55905a76d3b44a2c
yu.pan <yupan@yupandeMac-mini.local>
2025-03-15 13:08:47 +0800

    添加忽略文件

--- src/test/java/com/panyu/yupictureback/YuPictureBackApplicationTests.java
+++ src/test/java/com/panyu/yupictureback/YuPictureBackApplicationTests.java
@@ -1,32 +1,94 @@
 package com.panyu.yupictureback;
 
-import cn.hutool.core.util.RandomUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientConfigurationBuilder;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.consumer.ConsumeResult;
+import org.apache.rocketmq.client.apis.consumer.FilterExpression;
+import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
+import org.apache.rocketmq.client.apis.consumer.PushConsumer;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
+
 @SpringBootTest
+@Slf4j
 class YuPictureBackApplicationTests {
 
-
     @Test
-    void contextLoads() {
+    public void test1() throws ClientException {
+        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8080;xxx:8081
+        // 此处为示例，实际使用时请替换为真实的 Proxy 地址和端口
+        String endpoint = "localhost:8081";
+        // 消息发送的目标Topic名称，需要提前创建。
+        String topic = "TestTopic";
+        ClientServiceProvider provider = ClientServiceProvider.loadService();
+        ClientConfigurationBuilder builder = ClientConfiguration.newBuilder().setEndpoints(endpoint);
+        ClientConfiguration configuration = builder.build();
+        // 初始化Producer时需要设置通信配置以及预绑定的Topic。
+        Producer producer = provider.newProducerBuilder()
+                .setTopics(topic)
+                .setClientConfiguration(configuration)
+                .build();
+        // 普通消息发送。
+        Message message = provider.newMessageBuilder()
+                .setTopic(topic)
+                // 设置消息索引键，可根据关键字精确查找某条消息。
+                .setKeys("messageKey")
+                // 设置消息Tag，用于消费端根据指定Tag过滤消息。
+                .setTag("messageTag")
+                // 消息体。
+                .setBody("messageBody".getBytes())
+                .build();
+        try {
+            // 发送消息，需要关注发送结果，并捕获失败等异常。
+            SendReceipt sendReceipt = producer.send(message);
+            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+        } catch (ClientException e) {
+            log.error("Failed to send message", e);
+        }
+        // producer.close();
     }
 
-
     @Test
-    void test1(){
-        String s = RandomUtil.randomString(4);
-        for (int i = 0; i < 100; i++) {
-            System.out.println(RandomUtil.randomString("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 4));
-        }
-    }
+    public void test2() throws ClientException, InterruptedException {
 
-    @Test
-    void importData(){
-        int num = 3000;
-        for (int i = 0; i < 1000; i++) {
-            num++;
-            System.out.println(num);
-        }
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8080;xxx:8081
+        // 此处为示例，实际使用时请替换为真实的 Proxy 地址和端口
+        String endpoints = "localhost:8081";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+                .setEndpoints(endpoints)
+                .build();
+        // 订阅消息的过滤规则，表示订阅所有Tag的消息。
+        String tag = "*";
+        FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
+        // 为消费者指定所属的消费者分组，Group需要提前创建。
+        String consumerGroup = "YourConsumerGroup";
+        // 指定需要订阅哪个目标Topic，Topic需要提前创建。
+        String topic = "TestTopic";
+        // 初始化PushConsumer，需要绑定消费者分组ConsumerGroup、通信参数以及订阅关系。
+        PushConsumer pushConsumer = provider.newPushConsumerBuilder()
+                .setClientConfiguration(clientConfiguration)
+                // 设置消费者分组。
+                .setConsumerGroup(consumerGroup)
+                // 设置预绑定的订阅关系。
+                .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
+                // 设置消费监听器。
+                .setMessageListener(messageView -> {
+                    // 处理消息并返回消费结果。
+                    log.info("Consume message successfully, messageId={}", messageView.getMessageId());
+                    return ConsumeResult.SUCCESS;
+                })
+                .build();
+        Thread.sleep(Long.MAX_VALUE);
+        // 如果不需要再使用 PushConsumer，可关闭该实例。
+        // pushConsumer.close();
     }
 }
